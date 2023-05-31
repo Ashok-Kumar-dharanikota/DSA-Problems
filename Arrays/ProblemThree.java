f900e7fa9e630b304d5926701c8d784558ad08d6
Ashok-Kumar-dharanikota <ashok81432@gmail.com>
2023-05-31 16:27:44 +0530

    third commit

---
+++ Arrays/ProblemThree.java
@@ -0,0 +1,23 @@
+class ProblemThree {
+
+    public static void main(String[] args) {
+        
+        int Arr[] = {2, 4, 8, 10, 12, 14};
+        int n = Arr.length;
+
+        System.out.println(findMissing(Arr, n));
+
+
+    }
+
+
+   static int findMissing(int[] arr, int n) {
+        // code here
+        int a=arr[0];
+        int d=(arr[n-1]-a)/n;
+        for(int i=0;i<n;i++){
+            if((a+i*d)!=arr[i])return (a+i*d);
+        }
+        return -1;
+    }
+}
