f821f9c1e855e1571231af972dcf7cedd5ada30e
Ashok-Kumar-dharanikota <ashok81432@gmail.com>
2023-06-03 15:59:02 +0530

    binary sorting

---
+++ Arrays/Problem_6.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+/**
+ * Problem_6
+ */
+public class Problem_6 {
+
+    public static void main(String[] args) {
+        //sorting a binary array
+        int[] arr = {0,1,0,1,1,1,0,0};
+
+        Arrays.sort(arr);
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
